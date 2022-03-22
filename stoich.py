7911f261d4a5a9db8b6881e4178a037da874c93d
Jacob Olson <50496441+olsonjac@users.noreply.github.com>
2022-03-22 14:54:43 -0400

    Update stoich.py
    Commented out the main points of the code.

--- stoich.py
+++ stoich.py
@@ -1,5 +1,7 @@
+#constant varibale used for converting atoms to moles
 avogadro = float(6.02 * 10**23)
 
+#gram to gram conversion that asked for the necessary variable inputs
 def gram_gram():
   r1 = input("enter the given reactant symbol: ")
   r1a = float(input("enter the amount in grams of the given reactant: "))
@@ -41,10 +43,12 @@ def gram_Liter():
   answer = (r1a/r1m)*(r2c/r1c)*(22.4)
   print(f"{answer} Liters of {r2} will be made.")
 
+#the "eval" expression is used to convert a written expression in scientific notation to a useable variable
 def atom_atom():
   r1 = input("enter the given reactant symbol: ")
   r1a = input("enter the amount of the given reactant in atoms(use \"^\" for exponents): ")
   r1a = r1a.replace("^","**")
+  r1a = r1a.replace(" ","")
   r1a = eval(r1a)
   r1c = int(input("enter the coefficient in front of the given reactant: "))
   r2 = input("enter the symbol of the desired product: ")
@@ -52,7 +56,7 @@ def atom_atom():
   answer = (float(r1a)/avogadro)*(r2c/r1c)*(avogadro)
   print(f"{r1} will make {answer} atoms of {r2}") 
 
-
+#atoms to mole conversion function.
 def atom_mol():
   r1 = input("enter the given reactant symbol: ")
   r1a = input("enter the amount of atoms of the given reactant (use \"^\" for exponents): ")
@@ -64,6 +68,7 @@ def atom_mol():
   answer = (r1a/avogadro)*(r2c/r1c)
   print(f"there are {answer} moles of {r2}")
 
+#moles to atom conversion
 def mol_atom():
   r1 = input("enter the given reactant symbol: ")
   r1a = float(input("enter the moles of the given reactant(use \"^\" for exponents): "))
@@ -72,7 +77,7 @@ def mol_atom():
   r2c = int(input("enter the coefficient in front of the desired product: "))
   answer = (r1a)*(r2c/r1c)*(avogadro)
   print(f"there are {answer} atoms of {r2}")
-
+#moles to Liter conversion
 def mol_L():
   r1 = input("enter the given reactant symbol: ")
   r1a = float(input("enter the moles of the given reactant(use \"^\" for exponents): "))
@@ -81,7 +86,8 @@ def mol_L():
   r2c = int(input("enter the coefficient in front of the desired product: "))
   answer = (r1a)*(r2c/r1c)*(22.4)
   print(f"there are {answer} Liters of {r1}")
-    
+
+#Atom to liter conversion    
 def atom_L():
   r1 = input("enter the given reactant symbol: ")
   r1a = input("enter the amount of atoms of the given reactant (use \"^\" for exponents): ")
@@ -93,6 +99,7 @@ def atom_L():
   answer = (float(r1a)/avogadro)*(r2c/r1c)*(22.4)
   print(f"there are {answer} Liters of {r2}")
 
+#moles to moles conversion
 def mol_mol():
   r1 = input("enter the given reactant symbol: ")
   r1a = float(input("enter the moles of the given reactant: "))
@@ -102,6 +109,7 @@ def mol_mol():
   answer = (r1a)*(r2c/r1c)
   print(f"there are {answer} moles of {r2}")
 
+#Liters to moles conversion
 def Liter_mol():
   r1 = input("enter the given reactant symbol: ")
   r1a = float(input("enter the amount in Liters of the given reactant: "))
@@ -111,6 +119,7 @@ def Liter_mol():
   answer = (r1a/22.4)*(r2c/r1c)
   print(f"there are {answer} moles of {r2}")
 
+#Liters to atoms conversion
 def Liter_atom():
   r1 = input("enter the given reactant symbol: ")
   r1a = float(input("enter the amount in Liters of the given reactant: "))
@@ -120,6 +129,7 @@ def Liter_atom():
   answer = (r1a/22.4)*(r2c/r1c)*(avogadro)
   print(f"there are {answer} atoms of {r2}")
   
+#Liters to liters conversion
 def Liter_Liter():
   r1 = input("enter the given reactant symbol: ")
   r1a = float(input("enter the amount in Liters of the given reactant: "))
@@ -129,13 +139,18 @@ def Liter_Liter():
   answer = (r1a/22.4)*(r2c/r1c)*(22.4)
   print(f"there are {answer} Liters of {r2}")
 
+#initializes user input for the while loop below
 initial = ""
 final = ""
+
+#while loop that controls the main body of the code
 while True:
   
   initial = input("enter what units you START with(grams(g),moles(m),Liters(L),atoms(a)):")
   final = input("enter what units you need at the END (grams(g),moles(m),Liters(L),atoms(a)): ") 
-  
+
+#if statements that check for which function to implement based on the initial and final variables.
+
   if final == "a" and initial == "a":
     atom_atom()
     break
@@ -175,6 +190,7 @@ while True:
   elif initial == "g" and final == "g":
     gram_gram()
     break
+  #informs user they did not enter the correct option
   else:
     print("make sure you choose from the available options(g,a,L,m)")
     print("")
